13f15b17842a48e96ad028efd4e714bbe47bdc64
yt <113787435+rt234cw@users.noreply.github.com>
2026-04-04 15:31:47 +0800

    revisiting the security rules

--- src/main/java/com/codewithmosh/store/auth/SecurityConfig.java
+++ src/main/java/com/codewithmosh/store/auth/SecurityConfig.java
@@ -77,6 +77,10 @@ public class SecurityConfig {
                                 "/webjars/**").permitAll()
                         //這個users endpoint只允許POST method
                         .requestMatchers(HttpMethod.POST,"/users").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/products/**").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/products/**").hasRole(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.PUT,"/products/**").hasRole(Role.ADMIN.name())
+                        .requestMatchers(HttpMethod.DELETE,"/products/**").hasRole(Role.ADMIN.name())
                         .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST,"/auth/refresh").permitAll()
                         .requestMatchers(HttpMethod.POST,"/checkout/webhook").permitAll()
